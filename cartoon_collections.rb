fabd97cd1f18eb94f31b56ce4bb14c31ed1224e5
Dustin Blanton <dustinblntn@gmail.com>
2020-05-14 21:10:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,7 +36,7 @@ ingredients = ["garlic", "rosemary", "bread"]
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-   array.include?(cheese_types)
+  # array.include?(cheese_types)
     
       
     
